faedcd0d7ecbc62087785ae47c7e552f5c343d18
Dodalpaga <dorian.voydie@gmail.com>
2024-03-06 23:53:28 +0100

    Update

--- src/components/utils/Header.jsx
+++ src/components/utils/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../css/Header.css';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 
 export default function Header() {
